eee4a7e2f7accdd04b5bbe12f4b4a3b1b9a29e22
sqz777 <sqz777isme@gmail.com>
2018-01-10 21:38:00 +0800

    submit and pass

--- Codewars_FindTheDivisors/UnitTest1.cs
+++ Codewars_FindTheDivisors/UnitTest1.cs
@@ -43,6 +43,13 @@ namespace Codewars_FindTheDivisors
         {
             CollectionAssert.AreEqual(new int[] { 11, 23 }, Kata.Divisors(253));
         }
+
+        [TestMethod]
+        public void Input_99999999_SHould__()
+        {
+            CollectionAssert.AreEqual(new int[] { 3, 9, 11, 33, 73, 99, 101, 137, 219, 303, 411, 657, 803, 909, 1111, 1233, 1507, 2409, 3333, 4521, 7227, 7373, 9999, 10001, 13563, 13837, 22119, 30003, 41511, 66357, 81103, 90009, 110011, 124533, 152207, 243309, 330033, 456621, 729927, 990099, 1010101, 1369863, 3030303, 9090909, 11111111, 33333333 }, Kata.Divisors(99999999));
+        }
+
     }
 
     public class Kata
@@ -55,11 +62,12 @@ namespace Codewars_FindTheDivisors
                 if (num % i == 0)
                 {
                     result.Add(i);
-                    if (num / i != i)
-                        result.Add(num / i);
+                    result.Add(num / i);
                 }
             }
-            return result.Count > 0 ? result.OrderBy(x => x).ToArray() : null;
+            return result.Count > 0 ? result.OrderBy(x => x).Distinct().ToArray() : null;
+            //var result = Enumerable.Range(2, num / 2).Where(x => num % x == 0).ToArray();
+            //return result.Length > 0 ? result : null;
         }
     }
 }
